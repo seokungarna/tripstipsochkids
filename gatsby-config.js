e990e6bb99b5e011ce457bbe5cd114280d21a9b9
kungjk <jerker@kenntoft.se>
2023-11-07 13:27:14 +0100

    google tag m4

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
         /*id: 'GTM-add_your_tag_here',*/
-        id: 'UA-125733152-2',
+        id: 'G-P75GRS6SZ0',
         includeInDevelopment: false
       }
     },
